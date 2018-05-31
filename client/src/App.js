f10c9f13fb677fabb54fa64da10259826ed027d5
Parker Pestana <parkerpestana@gmail.com>
2018-05-30 20:32:33 -0700

    Client and server talk to eachother. Adds some db boilerplate code

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    yoyo_response: ""
+  };
+
+  componentDidMount() {
+    this.callApi("/api/yoyo")
+      .then((res) => this.setState({ yoyo_response: res.text }))
+      .catch((err) => console.log(err));
+  }
+
+  callApi = async (url) => {
+    const response = await fetch(url);
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +29,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.yoyo_response}</p>
       </div>
     );
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/api/yoyo", function(req, res, next) {
+  res.send({ text: "Yo Yo whatup!" });
 });
 
 module.exports = router;
